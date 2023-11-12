f71fc2926cbf7947426409096402f337fe80f9e8
simrit <simritn@student.ubc.ca>
2023-11-11 20:32:54 -0800

    Created a frontend demo of backend connection using Studio Created .gitignore file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react';
+import 'tailwindcss/tailwind.css';
 
 
 function App() {
@@ -17,22 +18,22 @@ function App() {
 
     return (
         <div className={`px-[10%] py-10`}>
-            <h1 className={`text-lg`}> Streaming Services </h1>
+            <h1 className={`text-3xl font-bold text-white pb-10`}> Streaming Services </h1>
 
-            <table className={`table-auto`}>
-                <thead>
+            <table className={`w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400`}>
+                <thead className={`text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400`}>
                     <tr>
-                        <th>Name</th>
-                        <th>Headquarters</th>
-                        <th>Creation Date</th>
+                        <th scope={`col`} className={`px-6 py-3`}>Name</th>
+                        <th scope={`col`} className={`px-6 py-3`}>Headquarters</th>
+                        <th scope={`col`} className={`px-6 py-3`}>Creation Date</th>
                     </tr>
                 </thead>
                 <tbody>
                     {data.map((item: any, index) => (
-                        <tr key={index}>
-                            <td>{item.studioName}</td>
-                            <td>{item.headquarter}</td>
-                            <td>{item.creationDate}</td>
+                        <tr key={index} className={`bg-white border-b dark:bg-gray-800 dark:border-gray-700`}>
+                            <td scope={`row`} className={`px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white`}>{item.studioName}</td>
+                            <td className={`px-6 py-4`}>{item.headquarter}</td>
+                            <td className={`px-6 py-4`}>{item.creationDate}</td>
                         </tr>
                     ))}
                 </tbody>
