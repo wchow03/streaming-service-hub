2f9424c2b2af9016e7c1cdceef98a05a047e4795
William <williamchow604@gmail.com>
2023-11-29 11:43:03 -0800

    Added alerts for successfully or unseccessfully registering account

--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -1,12 +1,20 @@
 import {Link, useNavigate} from "react-router-dom";
 import {useState} from "react";
+import {Alert, DatePicker, DatePickerProps, Input} from "antd";
 
 export default function Register() {
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [birthday, setBirthday] = useState("");
+    const [emailRegisteredSuccess, setEmailRegisteredSuccess] = useState(false);
+    const [emailRegisteredError, setEmailRegisteredError] = useState(false);
     const navigate = useNavigate();
+
+    const handleDateChange: DatePickerProps['onChange'] = (_date: any, dateString: any) => {
+        console.log(dateString);
+        setBirthday(dateString);
+    };
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
@@ -16,6 +24,15 @@ export default function Register() {
             email: email,
             birthday: birthday
         };
+        // await fetch("http://localhost:8080/api/authenticate/register", {
+        //     method: "POST",
+        //     headers: {
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify(body)
+        // })
+        //     .then(() => navigate("/"))
+        //     .catch(() => alert("Error registering account"));
         await fetch("http://localhost:8080/api/authenticate/register", {
             method: "POST",
             headers: {
@@ -23,12 +40,22 @@ export default function Register() {
             },
             body: JSON.stringify(body)
         })
-            .then(() => navigate("/"))
+            .then((res) => {
+                if (res.status === 400) {
+                    setEmailRegisteredError(true);
+                } else {
+                    setEmailRegisteredSuccess(true);
+                    setTimeout(() => navigate("/"), 2000);
+                }
+            })
             .catch(() => alert("Error registering account"));
     }
 
 
     return (
+        <>
+            {emailRegisteredSuccess && <Alert banner className={"text-center"} message="Account successfully registered" type="success"/>}
+            {emailRegisteredError && <Alert banner closable message="Error registering account, email may already be registered" type="error"/>}
         <div className={"d-flex justify-content-center align-items-center min-vh-100"}>
             <h1 className={"h1 text-white text-center mr-20"}>Streaming Service</h1>
             <form onSubmit={handleSubmit} className={"loginForm"}>
@@ -45,23 +72,34 @@ export default function Register() {
                     <label htmlFor={"floatingEmail"}>Email</label>
                 </div>
 
-                <div className={"form-floating mb-3"}>
-                    <input type={"password"} required className={"form-control"} id={"floatingPassword"}
-                           placeholder={"password"}
-                           onChange={(e) => setPassword(e.target.value)}/>
-                    <label htmlFor={"floatingPassword"}>Password</label>
-                </div>
+                {/*<div className={"form-floating mb-3"}>*/}
+                {/*    <Input.Password type={"password"} required className={"form-control"} id={"floatingPassword"}*/}
+                {/*           placeholder={"password"}*/}
+                {/*           onChange={(e) => setPassword(e.target.value)}/>*/}
+                {/*    <label htmlFor={"floatingPassword"}>Password</label>*/}
+                {/*</div>*/}
 
                 <div className={"form-floating mb-3"}>
-                    <input type={"date"} required className={"form-control"} id={"floatingBirthday"}
-                           placeholder={"birthday"}
-                           onChange={(e) => setBirthday(e.target.value)}/>
-                    <label htmlFor={"floatingBirthday"}>Birthday</label>
+                    <Input.Password required placeholder="Password"
+                                    className={"h-10"} id={"floatingPassword"}
+                                    onChange={(e: any) => setPassword(e.target.value)}/>
+                </div>
+
+                {/*<div className={"form-floating mb-3"}>*/}
+                {/*    <input type={"date"} required className={"form-control"} id={"floatingBirthday"}*/}
+                {/*           placeholder={"birthday"}*/}
+                {/*           onChange={(e) => setBirthday(e.target.value)}/>*/}
+                {/*    <label htmlFor={"floatingBirthday"}>Birthday</label>*/}
+                {/*</div>*/}
+
+                <div className={"form-floating mb-3"}>
+                    <DatePicker onChange={handleDateChange}/>
                 </div>
 
                 <button type={"submit"} className={"btn btn-primary w-100 mb-3"}>Register</button>
                 <Link to={"/"} className={"btn btn-secondary w-100"}>Back to Log in</Link>
             </form>
         </div>
+            </>
     );
 }
