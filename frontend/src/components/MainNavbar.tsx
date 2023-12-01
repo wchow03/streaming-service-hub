7c91abf57aa3cdb06b82ead8801ea4b35d8d7aae
simrit <simritn@student.ubc.ca>
2023-12-01 10:34:06 -0800

    Changed account settings to be an icon Fixed text center issues on logout button Fixed sizing issue with date input in registration

--- frontend/src/components/MainNavbar.tsx
+++ frontend/src/components/MainNavbar.tsx
@@ -66,12 +66,40 @@ export default function MainNavbar() {
                               onClick={hide}>Studios</Link>
                     </div>
 
-                    <Link className={"text-white hover:opacity-70"} to={"/accountSettings"}
-                          onClick={hide}>Account&nbsp;Settings</Link>
+                    <Link className={"text-white hover:opacity-70"}
+                          title={"Account Settings"}
+                          to={"/accountSettings"}
+                          onClick={hide}>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width={35}
+                            height={35}
+                            fill="none"
+                            viewBox="0 0 24 24"
+                        >
+                            <path
+                                stroke="#fff"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={1.5}
+                                d="M9.752 12.013c0-.954.657-1.776 1.57-1.962a1.94 1.94 0 0 1 2.181 1.196 2.03 2.03 0 0 1-.717 2.428 1.914 1.914 0 0 1-2.463-.248 2.026 2.026 0 0 1-.571-1.414Z"
+                                clipRule="evenodd"
+                            />
+                            <path
+                                stroke="#fff"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth={1.5}
+                                d="M10.308 5.468c-.014-.52.249-1.006.686-1.27a1.364 1.364 0 0 1 1.415 0c.437.264.7.75.686 1.27v.717a5.81 5.81 0 0 1 2.785 1.628l.555-.334a1.35 1.35 0 0 1 1.061-.144c.359.1.662.345.843.678a1.49 1.49 0 0 1-.51 1.987l-.517.31a6.187 6.187 0 0 1 0 3.4l.517.312c.67.41.897 1.296.51 1.99-.181.334-.485.577-.843.677-.358.1-.741.049-1.061-.144l-.555-.334a5.808 5.808 0 0 1-2.786 1.63v.717c.014.52-.249 1.006-.686 1.27a1.364 1.364 0 0 1-1.415 0c-.437-.264-.7-.75-.686-1.27v-.717a5.808 5.808 0 0 1-2.785-1.628l-.555.334a1.35 1.35 0 0 1-1.06.143 1.387 1.387 0 0 1-.843-.677 1.49 1.49 0 0 1 .51-1.987l.517-.311a6.187 6.187 0 0 1 0-3.4l-.517-.311a1.49 1.49 0 0 1-.51-1.991c.181-.333.485-.577.843-.677.358-.1.741-.048 1.061.145l.555.334a5.81 5.81 0 0 1 2.785-1.631v-.716Z"
+                                clipRule="evenodd"
+                            />
+                        </svg>
+                    </Link>
 
                     <Link
                         className={"text-white max-md:flex max-md:w-full max-md:text-center uppercase font-bold hover:bg-red-800 basis-1/12  bg-red-500 py-1 md:px-3 rounded-md transition-colors duration-150"}
-                        to={"/"} onClick={hide}><p className={`max-md:w-full max-md:m-auto`}>Log&nbsp;out</p></Link>
+                        to={"/"} onClick={hide}><p
+                        className={`max-md:w-full max-md:m-auto text-center`}>Log&nbsp;out</p></Link>
                 </div>
             </nav>
         </>
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -54,52 +54,56 @@ export default function Register() {
 
     return (
         <>
-            {emailRegisteredSuccess && <Alert banner className={"text-center"} message="Account successfully registered" type="success"/>}
-            {emailRegisteredError && <Alert banner closable message="Error registering account, email may already be registered" type="error"/>}
-        <div className={"d-flex justify-content-center align-items-center min-vh-100"}>
-            <h1 className={"h1 text-white text-center mr-20"}>Streaming Service</h1>
-            <form onSubmit={handleSubmit} className={"loginForm"}>
-                <div className={"form-floating mb-3"}>
-                    <input type={"text"} required className={"form-control"} id={"floatingUser"} placeholder={"bob123"}
-                           onChange={(e) => setUsername(e.target.value)}/>
-                    <label htmlFor={"floatingUser"}>Username</label>
-                </div>
+            {emailRegisteredSuccess &&
+                <Alert banner className={"text-center"} message="Account successfully registered" type="success"/>}
+            {emailRegisteredError &&
+                <Alert banner closable message="Error registering account, email may already be registered"
+                       type="error"/>}
+            <div className={"d-flex justify-content-center align-items-center min-vh-100"}>
+                <h1 className={"h1 text-white text-center mr-20"}>Streaming Service</h1>
+                <form onSubmit={handleSubmit} className={"loginForm"}>
+                    <div className={"form-floating mb-3"}>
+                        <input type={"text"} required className={"form-control"} id={"floatingUser"}
+                               placeholder={"bob123"}
+                               onChange={(e) => setUsername(e.target.value)}/>
+                        <label htmlFor={"floatingUser"}>Username</label>
+                    </div>
 
-                <div className={"form-floating mb-3"}>
-                    <input type={"email"} required className={"form-control"} id={"floatingEmail"}
-                           placeholder={"bob123@gmail.com"}
-                           onChange={(e) => setEmail(e.target.value)}/>
-                    <label htmlFor={"floatingEmail"}>Email</label>
-                </div>
+                    <div className={"form-floating mb-3"}>
+                        <input type={"email"} required className={"form-control"} id={"floatingEmail"}
+                               placeholder={"bob123@gmail.com"}
+                               onChange={(e) => setEmail(e.target.value)}/>
+                        <label htmlFor={"floatingEmail"}>Email</label>
+                    </div>
 
-                {/*<div className={"form-floating mb-3"}>*/}
-                {/*    <Input.Password type={"password"} required className={"form-control"} id={"floatingPassword"}*/}
-                {/*           placeholder={"password"}*/}
-                {/*           onChange={(e) => setPassword(e.target.value)}/>*/}
-                {/*    <label htmlFor={"floatingPassword"}>Password</label>*/}
-                {/*</div>*/}
+                    {/*<div className={"form-floating mb-3"}>*/}
+                    {/*    <Input.Password type={"password"} required className={"form-control"} id={"floatingPassword"}*/}
+                    {/*           placeholder={"password"}*/}
+                    {/*           onChange={(e) => setPassword(e.target.value)}/>*/}
+                    {/*    <label htmlFor={"floatingPassword"}>Password</label>*/}
+                    {/*</div>*/}
 
-                <div className={"form-floating mb-3"}>
-                    <Input.Password required placeholder="Password"
-                                    className={"h-10"} id={"floatingPassword"}
-                                    onChange={(e: any) => setPassword(e.target.value)}/>
-                </div>
+                    <div className={"form-floating mb-3"}>
+                        <Input.Password required placeholder="Password"
+                                        className={"h-10"} id={"floatingPassword"}
+                                        onChange={(e: any) => setPassword(e.target.value)}/>
+                    </div>
 
-                {/*<div className={"form-floating mb-3"}>*/}
-                {/*    <input type={"date"} required className={"form-control"} id={"floatingBirthday"}*/}
-                {/*           placeholder={"birthday"}*/}
-                {/*           onChange={(e) => setBirthday(e.target.value)}/>*/}
-                {/*    <label htmlFor={"floatingBirthday"}>Birthday</label>*/}
-                {/*</div>*/}
+                    {/*<div className={"form-floating mb-3"}>*/}
+                    {/*    <input type={"date"} required className={"form-control"} id={"floatingBirthday"}*/}
+                    {/*           placeholder={"birthday"}*/}
+                    {/*           onChange={(e) => setBirthday(e.target.value)}/>*/}
+                    {/*    <label htmlFor={"floatingBirthday"}>Birthday</label>*/}
+                    {/*</div>*/}
 
-                <div className={"form-floating mb-3"}>
-                    <DatePicker onChange={handleDateChange} placeholder={"Birthday"}/>
-                </div>
+                    <div className={"form-floating mb-3 w-full"}>
+                        <DatePicker className={`w-full py-2`} onChange={handleDateChange} placeholder={"Birthday"}/>
+                    </div>
 
-                <button type={"submit"} className={"btn btn-primary w-100 mb-3"}>Register</button>
-                <Link to={"/"} className={"btn btn-secondary w-100"}>Back to Log in</Link>
-            </form>
-        </div>
-            </>
+                    <button type={"submit"} className={"btn btn-primary w-100 mb-3"}>Register</button>
+                    <Link to={"/"} className={"btn btn-secondary w-100"}>Back to Log in</Link>
+                </form>
+            </div>
+        </>
     );
 }
