79c01b505e58a7e96f5937e10b4c18608672ab94
simrit <simritn@student.ubc.ca>
2023-12-01 01:53:24 -0800

    Updated alerts display

--- frontend/src/components/Alert.tsx
+++ frontend/src/components/Alert.tsx
@@ -17,7 +17,7 @@ export default function Alert({type, visible, children}: AlertProps) {
 
     return (
 
-        <div className={`${!visible && "hidden"} fixed w-full top-10 left-0 px-5`}>
+        <div className={`${!visible && "hidden"} fixed w-full top-0 left-0`}>
 
             {
                 getAlert(type, children)
@@ -35,7 +35,7 @@ export function getAlert(type: AlertType, children: any) {
         case AlertType.Success:
             return (
                 <div
-                    className={`flex flex-row bg-slate-600 w-full rounded py-2 px-3 border-1 border-green-500 items-center`}>
+                    className={`flex flex-row bg-transparent backdrop-blur w-full py-2 px-3 border-1 border-green-500 items-center`}>
                     <svg className={`flex-shrink-0 inline w-5 h-5 me-3 fill-green-300`}
                          aria-hidden="true"
                          xmlns="http://www.w3.org/2000/svg"
@@ -43,13 +43,13 @@ export function getAlert(type: AlertType, children: any) {
                         <path
                             d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
                     </svg>
-                    <p className={`text-green-400 font-bold`}>{children}</p>
+                    <p className={`text-green-400 w-full text-center`}>{children}</p>
                 </div>
             );
         case AlertType.Error:
             return (
                 <div
-                    className={`flex flex-row bg-slate-600 w-full rounded py-2 px-3 border-1 border-red-500 items-center`}>
+                    className={`flex flex-row bg-transparent backdrop-blur w-full py-2 px-3 border-1 border-red-500 items-center`}>
                     <svg className={`flex-shrink-0 inline w-5 h-5 me-3 fill-red-300`}
                          aria-hidden="true"
                          xmlns="http://www.w3.org/2000/svg"
@@ -57,13 +57,13 @@ export function getAlert(type: AlertType, children: any) {
                         <path
                             d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
                     </svg>
-                    <p className={`text-red-400 font-bold`}>{children}</p>
+                    <p className={`text-red-400 w-full text-center`}>{children}</p>
                 </div>
             );
         case AlertType.Warning:
             return (
                 <div
-                    className={`flex flex-row bg-slate-600 w-full rounded py-2 px-3 border-1 border-yellow-500 items-center`}>
+                    className={`flex flex-row bg-transparent backdrop-blur w-full py-2 px-3 border-1 border-yellow-500 items-center`}>
                     <svg className={`flex-shrink-0 inline w-5 h-5 me-3 fill-yellow-300`}
                          aria-hidden="true"
                          xmlns="http://www.w3.org/2000/svg"
@@ -71,13 +71,13 @@ export function getAlert(type: AlertType, children: any) {
                         <path
                             d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
                     </svg>
-                    <p className={`text-yellow-400 font-bold`}>{children}</p>
+                    <p className={`text-yellow-400 w-full text-center`}>{children}</p>
                 </div>
             );
         case AlertType.Info:
             return (
                 <div
-                    className={`flex flex-row bg-slate-600 w-full rounded py-2 px-3 border-1 border-blue-500 items-center`}>
+                    className={`flex flex-row bg-transparent backdrop-blur w-full py-2 px-3 border-1 border-blue-500 items-center`}>
                     <svg className={`flex-shrink-0 inline w-5 h-5 me-3 fill-blue-300`}
                          aria-hidden="true"
                          xmlns="http://www.w3.org/2000/svg"
@@ -85,7 +85,7 @@ export function getAlert(type: AlertType, children: any) {
                         <path
                             d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
                     </svg>
-                    <p className={`text-blue-400 font-bold`}>{children}</p>
+                    <p className={`text-blue-400 w-full text-center`}>{children}</p>
                 </div>
             );
         default:
